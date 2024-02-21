519372dfdf012053b7764072499e0a86bf7d37fa
NadymB <your_email_address@example.com>
2024-02-21 10:03:22 +0700

    create asset page

--- src/assets/icons/ArrowRightIcon.tsx
+++ src/assets/icons/ArrowRightIcon.tsx
@@ -1,28 +1,17 @@
 import React from "react";
 
 export const ArrowRightIcon = ({ color }: { color?: string }) => (
-  <svg
-    width="25"
-    height="24"
-    viewBox="0 0 25 24"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
+  <svg 
+    focusable="false" 
+    aria-hidden="true" 
+    viewBox="0 0 24 24" 
+    width={24}
+    height={24}
+    data-testid="ArrowBackIcon"
   >
-    <mask
-      id="mask0_7_269"
-      maskUnits="userSpaceOnUse"
-      x="0"
-      y="0"
-      width="25"
-      height="24"
-    >
-      <rect x="0.5" width="24" height="24" fill="#D9D9D9" />
-    </mask>
-    <g mask="url(#mask0_7_269)">
-      <path
-        d="M14.5 17.6538L8.84618 12L14.5 6.34619L15.5538 7.40002L10.9538 12L15.5538 16.6L14.5 17.6538Z"
-        fill={color}
-      />
-    </g>
+    <path 
+      d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20z"
+      fill={color}
+    ></path>
   </svg>
 );
---
+++ src/assets/icons/CheckIcon.tsx
@@ -0,0 +1,17 @@
+export const CheckIcon = () => {
+    return (
+        <svg 
+            focusable="false" 
+            aria-hidden="true" 
+            viewBox="0 0 24 24" 
+            width={24}
+            height={24}
+            data-testid="CheckIcon"
+        >
+            <path 
+                d="M9 16.17 4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"
+                fill="#3d5afe"
+            ></path>
+        </svg>
+    )
+}
