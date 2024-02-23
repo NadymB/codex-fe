fbb15fdbb176eda7f38738bc41d598940f1af205
Ty Ha <tyha@dayoneteams.com>
2024-02-23 17:46:36 +0700

    feat: fill data to chart

--- src/app/m/trade/page.tsx
+++ src/app/m/trade/page.tsx
@@ -1,7 +1,42 @@
+"use client";
+
 import { DefaultLayout } from "@/components/layouts/DefaultLayout";
 import { CandleStickChart } from "@/components/trade/CandleStickChart";
+import { tradeService } from "@/services/TradeService";
+import { PRICE_TYPE } from "@/utils/constants";
+import { ChartData } from "@/utils/type";
+import { useEffect, useState } from "react";
 
 const TradePage = () => {
+  const [chartData, setChartData] = useState<Array<ChartData>>([]);
+  const getChartData = async () => {
+    const response = await tradeService.getChartData(PRICE_TYPE.CRYPTO);
+
+    if (response.success) {
+      const formattedData: ChartData[] = response.data
+        .map((data: ChartData) => {
+          const dateObject = new Date(data.intervalStart);
+          const unixTimestamp = dateObject.getTime();
+
+          return [
+            unixTimestamp,
+            data.openingValue,
+            data.highestValue,
+            data.lowestValue,
+            data.closingValue,
+            48908700,
+          ];
+        })
+        .sort((a: number[], b: number[]) => a[0] - b[0]);
+
+      setChartData(formattedData);
+    }
+  };
+
+  useEffect(() => {
+    getChartData();
+  }, []);
+
   return (
     <DefaultLayout
       isShowMenubar={false}
@@ -9,7 +44,7 @@ const TradePage = () => {
       containerStyle="bg-[#13111a] dark:bg-[#13111a]"
     >
       <div className="text-white">heheh</div>
-      <CandleStickChart />
+      {chartData && <CandleStickChart data={chartData} />}
     </DefaultLayout>
   );
 };
