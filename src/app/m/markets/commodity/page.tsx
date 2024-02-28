4da98bffcf5f58e258a66882366d2082675b65c0
Ty Ha <tyha@dayoneteams.com>
2024-02-28 10:27:43 +0700

    fix: markets page

--- src/app/m/markets/commodity/page.tsx
+++ src/app/m/markets/commodity/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-/* eslint-disable react/jsx-key */
 import { Table } from "@/components/table/Table";
 import { DefaultLayout } from "@/components/layouts/DefaultLayout";
 import { Tab } from "@/components/layouts/Tab";
@@ -11,6 +10,7 @@ import { Button } from "@/components/table/Button";
 import { useEffect, useState } from "react";
 import { priceFeedService } from "@/services/PriceFeedService";
 import i18next from "i18next";
+import { Loading } from "@/components/Loading";
 
 const MarketPage = () => {
   const [data, setData] = useState([]);
@@ -20,11 +20,19 @@ const MarketPage = () => {
         PRICE_TYPE.COMMODITY
       );
       if (response.success) {
-        const mappedData = response.data.map((item: any) => {
+        const mappedData = response.data.map((item: any, index: number) => {
           return [
-            <TrandingCell tradingName={item.name} totalValue="99.14M" />,
-            <PriceCell usdtPrice={item.value} usdPrice={item.value} />,
-            <Button text="+3.06%" className="bg-[#54AF71]" />,
+            <TrandingCell
+              key={index}
+              tradingName={item.metadata.name.split("_")[0].toUpperCase()}
+              totalValue="99.14M"
+            />,
+            <PriceCell
+              key={index}
+              usdtPrice={item.value}
+              usdPrice={item.value}
+            />,
+            <Button key={index} text="+3.06%" className="bg-[#54AF71]" />,
           ];
         });
         setData(mappedData);
@@ -60,16 +68,22 @@ const MarketPage = () => {
       containerStyle="bg-[#000000] dark:bg-[#000000]"
     >
       <Tab options={marketTabOption} />
-      <div className="w-full p-3">
-        <Table
-          columns={[
-            `${i18next.t("marketPage.transaction")} / ${i18next.t("marketPage.tradingVolume")}`,
-            `${i18next.t("marketPage.price")}`,
-            `${i18next.t("marketPage.24HRisingDecline")}`,
-          ]}
-          data={data}
-        />
-      </div>
+      {data.length ? (
+        <div className="w-full p-3">
+          <Table
+            columns={[
+              `${i18next.t("marketPage.transaction")} / ${i18next.t("marketPage.tradingVolume")}`,
+              `${i18next.t("marketPage.price")}`,
+              `${i18next.t("marketPage.24HRisingDecline")}`,
+            ]}
+            data={data}
+          />
+        </div>
+      ) : (
+        <div className="h-screen">
+          <Loading />
+        </div>
+      )}
     </DefaultLayout>
   );
 };
