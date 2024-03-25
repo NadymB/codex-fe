f8d772e2baec4a3be3de311da4618f981f92394c
ThanhDatwd <nguyenthanhdatntd01@gmail.com>
2024-03-25 10:57:40 +0700

    fix:ui-trade

--- src/components/trade/TradingChartBar.tsx
+++ src/components/trade/TradingChartBar.tsx
@@ -12,11 +12,11 @@ export const TradingChartBar = () => {
 
   const generateRandomHeights = () => {
     const newWidthLong = columnWidthLong.map(() =>
-      Math.floor(Math.random() * 100),
+      Math.floor(Math.random() * 100)
     );
     setColumnWidthLong(newWidthLong);
     const newWidthShort = columnWidthShort.map(() =>
-      Math.floor(Math.random() * 100),
+      Math.floor(Math.random() * 100)
     );
     setColumnWidthShort(newWidthShort);
   };
@@ -39,7 +39,11 @@ export const TradingChartBar = () => {
       </div>
       {columnWidthLong.map((width, index) => (
         <div key={index} className="flex relative justify-between">
-          <span className="text-[#55af72] text-[12px]">24.137</span>
+          <span className="text-[#55af72] text-[12px]">
+            {(Math.random() * 0.3 + 2134)
+              .toFixed(2)
+              .replace(/\d(?=(\d{3})+\.)/g, "$&,")}
+          </span>
           <span className="text-[#fff] text-[12px]">
             {(Math.random() * 1000 * width)
               .toFixed(4)
@@ -52,12 +56,22 @@ export const TradingChartBar = () => {
         </div>
       ))}
       <div className="flex flex-col my-1">
-        <span className="text-[#55af72] font-bold text-[16px]">24.137</span>
-        <span className="text-[#fff] text-[12px]">≈ 88.62 AED</span>
+        <span className="text-[#55af72] font-bold text-[16px]">
+          {(Math.random() * 0.3 + 2134)
+              .toFixed(2)
+              .replace(/\d(?=(\d{3})+\.)/g, "$&,")}
+        </span>
+        <span className="text-[#fff] text-[12px]">≈ {(Math.random() * 0.3 + 2134)
+              .toFixed(2)
+              .replace(/\d(?=(\d{3})+\.)/g, "$&,")} USDT</span>
       </div>
       {columnWidthShort.map((width, index) => (
         <div key={index} className="flex relative justify-between">
-          <span className="text-[#dd5350] text-[12px]">24.137</span>
+          <span className="text-[#dd5350] text-[12px]">
+            {(Math.random() * 0.3 + 2134)
+              .toFixed(2)
+              .replace(/\d(?=(\d{3})+\.)/g, "$&,")}
+          </span>
           <span className="text-[#fff] text-[12px]">
             {(Math.random() * 1000 * width)
               .toFixed(4)
