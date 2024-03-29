7cba1edadc4ab5e24bef54d8b9d4830dc15d2d0a
vodinhvien <vodinhvien.work@gmail.com>
2024-03-29 20:27:28 +0700

    fix: link

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: true,
   images: {
     domains: ["localhost", "codex-client-eight.vercel.app", "static.tradingcme.com", "tradingcme.com"],
   },
