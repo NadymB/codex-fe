68637f7584690697d20ca011810a65f57e35fe39
NadymB <dantrinh2792001@gmail.com>
2024-04-01 15:36:05 +0700

    seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,41 +1,15 @@
+"use client";
 import { ToastContainer } from "react-toastify";
 
 import { AuthProvider } from "@/components/authentication/AuthProvider";
 import { ChatProvider } from "@/providers/ChatProvider";
 import { ThemeProvider } from "@/providers/ThemeProvider";
 import { WebSocketProvider } from "@/providers/WebSocketProvider";
-import { getStaticURL } from "@/utils/constants";
 import i18next from "i18next";
-import { NextSeo } from "next-seo";
-import Head from "next/head";
 import React from "react";
 import "react-toastify/dist/ReactToastify.css";
 import "../../i18n";
 import "../styles/index.css";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Futures & Options Trading for Risk Management - CME Group",
-  description:
-    "Futures & Options Trading for Risk Management - CME Group",
-  openGraph: {
-    title: "Đại Việt sử ký",
-    description:
-      "Futures & Options Trading for Risk Management - CME Group",
-    url: `${getStaticURL()}`,
-    // siteName: "Next.js",
-    images: [
-      {
-        url: `${getStaticURL()}/assets/images/logo.png`, // Must be an absolute URL
-        width: 800,
-        height: 600,
-        alt: "Logo CME Group",
-      },
-    ],
-    locale: "en_US",
-    type: "website",
-  },
-};
 
 export default function RootLayout({
   children,
@@ -51,32 +25,8 @@ export default function RootLayout({
     }
   }
 
-
   return (
     <>
-      <Head>
-          <NextSeo
-            title="Futures & Options Trading for Risk Management - CME Group"
-            description="Futures & Options Trading for Risk Management - CME Group"
-            canonical={`${getStaticURL()}`}
-            openGraph={{
-              url: `${getStaticURL()}`,
-              title: "Futures & Options Trading for Risk Management - CME Group",
-              description:
-                "Futures & Options Trading for Risk Management - CME Group",
-              images: [
-                {
-                  url: `${getStaticURL()}/assets/images/cme.svg`,
-                  width: 800,
-                  height: 600,
-                  alt: "CME Group",
-                  type: "image/png",
-                },
-              ],
-              siteName: "Futures & Options Trading for Risk Management - CME Group",
-            }}
-          />
-        </Head>
       <html lang="en">
         <body suppressHydrationWarning={true}>
           <WebSocketProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,57 @@
 /* eslint-disable @next/next/no-img-element */
-"use client";
+import { getStaticURL } from "@/utils/constants";
+import { Metadata } from "next";
+import { NextSeo } from "next-seo";
+import Head from "next/head";
 import LandingPage from "./landing/page";
+
+export const metadata: Metadata = {
+  title: "Futures & Options Trading for Risk Management - CME Group",
+  description: "Futures & Options Trading for Risk Management - CME Group",
+  openGraph: {
+    title: "Đại Việt sử ký",
+    description: "Futures & Options Trading for Risk Management - CME Group",
+    url: `${getStaticURL()}`,
+    // siteName: "Next.js",
+    images: [
+      {
+        url: `${getStaticURL()}/assets/images/cme.svg`, // Must be an absolute URL
+        width: 800,
+        height: 600,
+        alt: "Logo CME Group",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+};
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <NextSeo
+          title="Futures & Options Trading for Risk Management - CME Group"
+          description="Futures & Options Trading for Risk Management - CME Group"
+          canonical={`${getStaticURL()}`}
+          openGraph={{
+            url: `${getStaticURL()}`,
+            title: "Futures & Options Trading for Risk Management - CME Group",
+            description:
+              "Futures & Options Trading for Risk Management - CME Group",
+            images: [
+              {
+                url: `${getStaticURL()}/assets/images/cme.svg`,
+                width: 800,
+                height: 600,
+                alt: "CME Group",
+              },
+            ],
+            siteName:
+              "Futures & Options Trading for Risk Management - CME Group",
+          }}
+        />
+      </Head>
       <LandingPage />
     </>
   );
