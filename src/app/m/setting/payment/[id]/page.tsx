7b907ff46fc57e316bdd6e73d63a53a261a4745d
NadymB <dantrinh2792001@gmail.com>
2024-04-10 11:44:18 +0700

    fix hide secure value withdraw

--- src/app/m/setting/payment/[id]/page.tsx
+++ src/app/m/setting/payment/[id]/page.tsx
@@ -60,16 +60,16 @@ const CreatePaymentPage = () => {
         if(response.data.type===WITHDRAW_TYPE.FIAT_CURRENCY){
             fiatCurrencyformik.setFieldValue("country", response.data.country)
             fiatCurrencyformik.setFieldValue("bankName", response.data.bankName)
-            fiatCurrencyformik.setFieldValue("bankAccount", response.data.bankAccount)
-            fiatCurrencyformik.setFieldValue("bankNumber", response.data.bankNumber)
+            // fiatCurrencyformik.setFieldValue("bankAccount", response.data.bankAccount)
+            // fiatCurrencyformik.setFieldValue("bankNumber", response.data.bankNumber)
             fiatCurrencyformik.setFieldValue("comment", response.data.comment)
             fiatCurrencyformik.setFieldValue("address", response.data.address)
-            fiatCurrencyformik.setFieldValue("nationalIdCard", response.data.nationalIdCard)
-            fiatCurrencyformik.setFieldValue("phoneNumber", response.data.phoneNumber)
+            // fiatCurrencyformik.setFieldValue("nationalIdCard", response.data.nationalIdCard)
+            // fiatCurrencyformik.setFieldValue("phoneNumber", response.data.phoneNumber)
         }
         else if (response.data.type===WITHDRAW_TYPE.CRYPTO_CURRENCY) {
            
-            cryptoCurrencyformik.setFieldValue("walletAddress", response.data.walletAddress)
+            // cryptoCurrencyformik.setFieldValue("walletAddress", response.data.walletAddress)
             cryptoCurrencyformik.setFieldValue("comment", response.data.comment)
         }
         if (response.data.cryptoCurrency) {
@@ -415,7 +415,7 @@ const CreatePaymentPage = () => {
                     placeholder={t("withdrawAccount.cryptocurrencyPlaceholder")}
                     className="w-full"
                     name="cryptoCurrency"
-                    value={`${currentCurrency?.acronym} (${currentCurrency?.name})`}
+                    value={currentCurrency?.acronym && currentCurrency?.name ? `${currentCurrency?.acronym} (${currentCurrency?.name})` : ""}
                     InputProps={{
                       endAdornment: (
                         <InputAdornment position="end">
